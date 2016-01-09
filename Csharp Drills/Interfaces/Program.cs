b285a87073c15f4b9123ec75a2c6f900aa7bee0a
Kent Gray <aggie12man@gmail.com>
2016-01-09 13:46:36 -0800

    C# Drills
    Intercaces

---
+++ Csharp Drills/Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+interface ICustomer1
+{
+    void Print1();
+}
+
+interface ICustomer2 : ICustomer1
+{
+    void Print2();
+}
+public class Customer : ICustomer2
+{
+    public void Print2()
+    {
+        Console.WriteLine("Print2");
+    }
+    public void Print1()
+    {
+        Console.WriteLine("Print1");
+    }
+}
+
+
+public class Program
+    {
+        public static void Main()
+        {
+        ICustomer1 Cust = new Customer();
+        Cust.Print1();
+        }
+    }
+
