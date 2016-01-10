7b070b6bf34ccb45e05a76e730e9f4fda7646f3f
Kent Gray <aggie12man@gmail.com>
2016-01-09 16:21:54 -0800

    C# Delegates
    C# Drills

---
+++ Csharp Drills/Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+public delegate void HelloFunctionDelegate(string Message);
+
+class Program
+    {
+        public static void Main()
+        {
+        HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+        del("Hello from Delegate");
+        }
+
+       public static void Hello(string strMessage)
+        {
+        Console.WriteLine(strMessage);
+        }
+    }
+
+
+
+
+
